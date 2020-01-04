532d4b16340588b4475dad75adc8dc164ff881da
7n4mbh <645574+7n4mbh@users.noreply.github.com>
2020-01-04 23:30:40 +0900

    Added a starting script for the gitbucket container to wait for MySQL DB being prepared.

---
+++ gitbucket/gitbucket.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+
+until mysql -uroot -h"$host" -e 'show databases' > /dev/null; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - starting gitbucket"
+exec java -jar /opt/gitbucket.war
