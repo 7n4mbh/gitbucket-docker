13217064ab8227577322b9e2c8937e33d1aabf24
7n4mbh <645574+7n4mbh@users.noreply.github.com>
2020-01-04 23:43:12 +0900

    Changed restart policy from 'always' to 'unless-stopped'

--- gitbucket/gitbucket.sh
+++ gitbucket/gitbucket.sh
@@ -5,7 +5,7 @@ set -e
 host="$1"
 
 until mysql -uroot -h"$host" -e 'show databases' > /dev/null; do
-  >&2 echo "MySQL is unavailable - sleeping"
+  >&2 echo "MySQL is unavailable - retrying"
   sleep 1
 done
 
