3eb33be71adf8a81f03d7ca0cea9809789c05fd9
7n4mbh <645574+7n4mbh@users.noreply.github.com>
2020-01-11 09:55:01 +0900

    Modified the log messages of backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,7 +3,7 @@
 # Name of an archived file to be created
 FILE_OUT=backup_`date +%Y%m%d`.tgz
 
-echo "Started Gitbucket Backup: $FILE_OUT"
+echo `date +"%Y/%m/%d %H:%M:%S %z"` "Started Gitbucket Backup: $FILE_OUT"
 
 # Create a temporary MySQL dump file
 mysqldump --single-transaction -uroot -hdb gitbucket > /mnt/from/mysql_gitbucket.dump
@@ -14,10 +14,10 @@ tar czf /mnt/to/$FILE_OUT -C /mnt/from repositories mysql_gitbucket.dump
 # Remove MySQL dump file
 rm -f /mnt/from/mysql_gitbucket.dump
 
-echo "Removing old backup files..."
+echo `date +"%Y/%m/%d %H:%M:%S %z"` "Removing old backup files..."
 
 # Delete old backup files (older than a week)
 # find /mnt/to -mtime +7 -name *tgz -print
 find /mnt/to -mtime +7 -name *tgz -exec rm -rf {} \;
 
-echo "Done."
+echo `date +"%Y/%m/%d %H:%M:%S %z"` "Done."
